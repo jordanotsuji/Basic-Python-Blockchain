1062122fd2c2e05209e70ead7fcdd4330d82fa33
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-07-25 11:19:37 -1000

    Added REST API

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -1,6 +1,10 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
+from textwrap import dedent
+from flask import Flask, jsonify
+
 # Blockchain.py
 # Features:
 #   Ability to add multiple nodes to the Blockchain
@@ -92,3 +96,33 @@ class Blockchain:
         guessString = f'{last_proof}{guess}'.encode()
         guessHash = hashlib.sha256(guessString).hexdigest()
         return guessHash[:4] == "0000"
+
+# Blockchain class definition end
+
+# Instantiate node
+app = Flask(__name__)
+
+# Generate uuid for this node (universally unique id protocol not based on central authority)
+node_identifier = str(uuid4()).replace('-', '')
+
+# Instantiate the blockchain
+blockchain = Blockchain()
+
+@app.route('/mine', methods=['GET'])
+def mine():
+    return "Mining a new block"
+
+@app.route('/transactions/new', methods=['POST'])
+def new_transaction():
+    return "Adding new transaction"
+
+@app.route('/chain', methods=['GET'])
+def full_chain():
+    response = {
+            'chain':blockchain.chain,
+            'length':len(blockchain.chain)
+            }
+    return jsonify(response), 200 
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
