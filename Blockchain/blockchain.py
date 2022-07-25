251cbfa7778f219b7c48af41965ddfdaad1780e5
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-07-24 15:53:28 -1000

    Basic class skeleton

--- Blockchain/blockchain.py
+++ Blockchain/blockchain.py
@@ -5,5 +5,34 @@
 #   Simple conflict resolution (two blocks solved at the same time)
 #   Trasactions with RSA encryption
 
+class Blockchain:
 
+    def __init__(self):
+        self.chain = []
+        self.transactions = []
 
+    def new_block(self):
+        # Creates a new block and ads it to the chain
+        """
+        Comment/long string but used as comments sometimes
+        """
+        pass
+
+    def new_transaction(self, sender, recipient, amount):
+        # Adds new transaction to the list of transactions 
+        self.transactions.append({
+            'sender':sender,
+            'recipient':recipient,
+            'amount':amount
+            })
+        return self.last_block['index'] + 1
+
+    @staticmethod
+    def hash(block):
+        #Hashes a block
+        pass
+
+    @property
+    def last_block(self):
+        # Returns the last block in the chain
+        return self.chain[-1]
