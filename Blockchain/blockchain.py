bc3e75caacfdf2c2bbfd284ca92b2651b54f28cc
jordanotsuji <18375877+jordanotsuji@users.noreply.github.com>
2022-07-24 15:01:38 -1000

    Basic files and comments with functionality

---
+++ Blockchain/blockchain.py
@@ -0,0 +1,9 @@
+# Blockchain.py
+# Features:
+#   Ability to add multiple nodes to the Blockchain
+#   Proof of Work
+#   Simple conflict resolution (two blocks solved at the same time)
+#   Trasactions with RSA encryption
+
+
+
---
+++ Blockchain_Client/blockchain_client.py
@@ -0,0 +1,6 @@
+# Blockchain Client
+# Features:
+#   Wallet generation using public/private key encryption (RSA)
+#   Transaction generation using RSA
+
+
